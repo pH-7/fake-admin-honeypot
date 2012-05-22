a6a780ca182b2fa35a04f8d2cf94e9c0191f4e3d
Pierre-Henry SORIA <ph_7@hotmail.be>
2012-05-22 11:53:04 +0200

    Many Changes

--- fake-admin-panel/forms/processing/LoginAdminFormProcessing.php
+++ fake-admin-panel/forms/processing/LoginAdminFormProcessing.php
@@ -1,14 +1,16 @@
 <?php
 namespace PH7;
-defined('PH7') or exit('Restricted access');
+use PH7\Framework\Ip\Ip;
 
 class LoginAdminFormProcessing extends Form {
 
     public function __construct() {
         parent::__construct();
 
+        (new Logger)->init($_POST);
+        
         sleep(6); // Security against brute-force attack and this will irritate the hacker
-        $this->session->set('captcha_admin_enabled',1); // Enable Captcha
+        $this->session->set('captcha_admin_enabled', 1); // Enable Captcha
         \PFBC\Form::setError('form_admin_login', t('"Email", "Username" or "Password" is Incorrect'));
     }
 
